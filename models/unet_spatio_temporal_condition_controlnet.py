b2e17fbb514de794d422fba86bcb345c95d40692
SmileBCZ <Smilezbc@users.noreply.github.com>
2025-10-31 11:18:58 +0800

    fix bug in unet add residual from controlnet

--- models/unet_spatio_temporal_condition_controlnet.py
+++ models/unet_spatio_temporal_condition_controlnet.py
@@ -448,15 +448,15 @@ class UNetSpatioTemporalConditionControlNetModel(ModelMixin, ConfigMixin, UNet2D
 
             down_block_res_samples += res_samples
 
-            new_down_block_res_samples = ()
+        new_down_block_res_samples = ()
 
-            for down_block_res_sample, down_block_additional_residual in zip(
-                down_block_res_samples, down_block_additional_residuals
-            ):
-                down_block_res_sample = down_block_res_sample + down_block_additional_residual
-                new_down_block_res_samples = new_down_block_res_samples + (down_block_res_sample,)
+        for down_block_res_sample, down_block_additional_residual in zip(
+            down_block_res_samples, down_block_additional_residuals
+        ):
+            down_block_res_sample = down_block_res_sample + down_block_additional_residual
+            new_down_block_res_samples = new_down_block_res_samples + (down_block_res_sample,)
 
-            down_block_res_samples = new_down_block_res_samples
+        down_block_res_samples = new_down_block_res_samples
 
         
         # 4. mid
